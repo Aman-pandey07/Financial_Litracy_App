3bf3f5c02343e965ea8461b4eaa246625d7a4949
Aman-pandey07 <99860796+Aman-pandey07@users.noreply.github.com>
2024-08-21 22:34:09 +0530

    left navigation drawer work left

---
+++ app/src/main/java/com/aman/financial_litracy_app/homescreen/LeftNavigationDrawer.kt
@@ -0,0 +1,16 @@
+package com.aman.financial_litracy_app.homescreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LeftNavigationDrawer() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LeftNavigationDrawerPreview() {
+    LeftNavigationDrawer()
+}

3bdd6f0c786ab555a6acbfd2ea487a7fc509928f
Aman-pandey07 <99860796+Aman-pandey07@users.noreply.github.com>
2024-08-24 18:53:07 +0530

    color pallet and them changed

--- app/src/main/java/com/aman/financial_litracy_app/ui/theme/Color.kt
+++ app/src/main/java/com/aman/financial_litracy_app/ui/theme/Color.kt
@@ -2,10 +2,27 @@ package com.aman.financial_litracy_app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val Blue80 = Color(0xFF35B0FC)
+val PurpleGrey80 = Color(0xFF0D0F44)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+
+//Gray shade
+val Gray1 = Color(0xFFF8F9FA)
+val Gray2 = Color(0xFFF1F3F5)
+val Gray3 = Color(0xFFE9ECEF)
+val Gray4 = Color(0xFFDEE2E6)
+val Gray5 = Color(0xFFCED4DA)
+val Gray6 = Color(0xFFACB5BD)
+val Gray7 = Color(0xFF858E96)
+val Gray8 = Color(0xFF495057)
+val Gray9 = Color(0xFF343A40)
+val Gray10 = Color(0xFF212529)
+
+//State color
+val Success = Color(0xFF38C976)
+val Error = Color(0xFFFF3819)
+val Warning = Color(0xFFFCC132)
+
+//tertiary
 val Pink40 = Color(0xFF7D5260)

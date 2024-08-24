033a70f1f6619547707c6ae482c4dc18bb4a7c48
Aman-pandey07 <99860796+Aman-pandey07@users.noreply.github.com>
2024-08-24 19:39:51 +0530

    color pallet and them changed

--- app/src/main/java/com/aman/financial_litracy_app/leftnavigationdrawer/TermsAndConditionScreen.kt
+++ app/src/main/java/com/aman/financial_litracy_app/leftnavigationdrawer/TermsAndConditionScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -18,6 +20,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -63,7 +66,18 @@ fun TermsAndConditionScreen() {
                 TextCard(title = "6. Clause 6", content = "The sun dappled through the leaves as I strolled along the winding path. Birdsong filled the air, and the gentle breeze carried the sweet scent of wildflowers. A gentle smile crept across my face as I savored the tranquility of the moment.")
                 TextCard(title = "7. Clause 7", content = "The sun dappled through the leaves as I strolled along the winding path. Birdsong filled the air, and the gentle breeze carried the sweet scent of wildflowers. A gentle smile crept across my face as I savored the tranquility of the moment.")
                 TextCard(title = "8. Clause 8", content = "The sun dappled through the leaves as I strolled along the winding path. Birdsong filled the air, and the gentle breeze carried the sweet scent of wildflowers. A gentle smile crept across my face as I savored the tranquility of the moment.")
-
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        contentColor = Color.White,
+                        disabledContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                        disabledContentColor = Color.White
+                    )
+                ) {
+                    Text(text = "Accept & Continue")
+                }
             }
         }
     )

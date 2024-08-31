8d06014a17edf80bf58f15f07bf8fd25fc5e0c0c
Aman-pandey07 <99860796+Aman-pandey07@users.noreply.github.com>
2024-08-31 22:45:25 +0530

    firebase and theme done entirely onlu cource data passing left

--- app/src/main/java/com/aman/financial_litracy_app/homescreen/HomeScreen.kt
+++ app/src/main/java/com/aman/financial_litracy_app/homescreen/HomeScreen.kt
@@ -152,6 +152,7 @@ fun HomeScreen(navController: NavController,authViewModel: AuthViewModel){
                     shape = RoundedCornerShape(10.dp)// Adjust button padding
                 ) {
                     Text(text = "See All Courses", color = Color.White) // White text on primary background
+                    Text(text = "kuch nahi")
                 }
 
 
